8d1deff4d84372e7227f95009f0815e9159499cb
James Powell <blackicec@gmail.com>
2015-03-29 21:02:55 -0400

    Set the motion controller to point to the 127.0.0.1 address instead of the hardcoded DNS name of a test machine I was using.

--- MotionController.php
+++ MotionController.php
@@ -1,5 +1,4 @@
 <?php
-    echo 'Hello World';
 
     /*****************************************/
     echo "<h2>TCP/IP Connection</h2>\n";
@@ -8,14 +7,15 @@
     $service_port = 1550;
 
     /* Get the IP address for the target host. */
-    $address = 'mediahost.local'; // or 127.0.0.1
+    //$address = 'mediahost.local';
+    $address = '127.0.0.1';
 
     /* Create a TCP/IP socket. */
     $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
     if ($socket === false) {
-            echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "</br>";
+            echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "<br>";
     } else {
-            echo "OK.</br>";
+            echo "OK.<br>";
     }
 
     echo "Attempting to connect to '$address' on port '$service_port'...";
@@ -23,14 +23,14 @@
     if ($result === false) {
             echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
     } else {
-            echo "OK.\n";
+            echo "OK.<br>";
     }
     
     $data = $_GET['data'];
 
-    echo 'Sending data to server now . . . </br>';
+    echo 'Sending data to server now . . . <br>';
     socket_write($socket, $data, strlen($data));
-    echo "OK.\n";
+    echo "OK.<br>";
 
     /*****************************************/
 
